7bfd3444681dfbb2b4ef01e30cf2b7a2cd551f55
Jan-Jan <jan.jan.code@gmail.com>
2017-04-24 13:46:26 +0200

    Remove default example

--- app/app.js
+++ app/app.js
@@ -14,12 +14,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
-import FontFaceObserver from 'fontfaceobserver'
 import { useScroll } from 'react-router-scroll'
 import 'sanitize.css/sanitize.css'
 
 // Import root app
-// import App from 'containers/App'
+import App from 'containers/App'
 
 // Import selector for `syncHistoryWithStore`
 import { makeSelectLocationState } from 'containers/App/selectors'
@@ -28,11 +27,11 @@ import { makeSelectLocationState } from 'containers/App/selectors'
 import LanguageProvider from 'containers/LanguageProvider'
 
 // Load the favicon, the manifest.json file and the .htaccess file
-/* eslint-disable import/no-webpack-loader-syntax */
+/* eslint-disable import/no-unresolved, import/extensions */
 import '!file-loader?name=[name].[ext]!./favicon.ico'
 import '!file-loader?name=[name].[ext]!./manifest.json'
-import 'file-loader?name=[name].[ext]!./.htaccess' // eslint-disable-line import/extensions
-/* eslint-enable import/no-webpack-loader-syntax */
+import 'file-loader?name=[name].[ext]!./.htaccess'
+/* eslint-enable import/no-unresolved, import/extensions */
 
 import configureStore from './store'
 
@@ -42,19 +41,8 @@ import { translationMessages } from './i18n'
 // Import CSS reset and Global Styles
 import './global-styles'
 
-// Import routes
-// import createRoutes from './routes';
-
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
-const openSansObserver = new FontFaceObserver('Open Sans', {})
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add('fontLoaded')
-}, () => {
-  document.body.classList.remove('fontLoaded')
-})
+// Import root routes
+import createRoutes from './routes'
 
 // Create redux store with history
 // this uses the singleton browserHistory provided by react-router
@@ -70,12 +58,11 @@ const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: makeSelectLocationState(),
 })
 
-/*
 // Set up the router, wrapping all Routes in the App component
 const rootRoute = {
   component: App,
   childRoutes: createRoutes(store),
-};
+}
 
 const render = (messages) => {
   ReactDOM.render(
@@ -93,55 +80,8 @@ const render = (messages) => {
       </LanguageProvider>
     </Provider>,
     document.getElementById('app')
-  );
-};
-*/
-
-const MOUNT_NODE = document.getElementById('app')
-
-const renderApp = (messages) => {
-  // eslint-disable-next-line global-require
-  const routes = require('./routes/index').default(store)
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={messages}>
-        <Router
-          {...{ history, routes }}
-          render={
-            // Scroll to top when going to a new page,
-            // imitating default browser behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
-    MOUNT_NODE
   )
 }
-
-const isDev = (process.env.NODE_ENV === 'development' && module.hot)
-
-const renderError = !isDev
-  ? () => {}
-  : (error) => {
-    // eslint-disable-next-line global-require
-    const RedBox = require('redbox-react').default
-
-    ReactDOM.render(<RedBox {...{ error }} />, MOUNT_NODE)
-  }
-
-const render = !isDev
-  ? (messages) => renderApp(messages)
-  : (messages) => {
-    try {
-      renderApp(messages)
-    } catch (error) {
-      console.error(error) // eslint-disable-line
-      renderError(error)
-    }
-  }
-
 
 // Hot reloadable translation json files
 if (module.hot) {
@@ -150,16 +90,7 @@ if (module.hot) {
   module.hot.accept('./i18n', () => {
     render(translationMessages)
   })
-
-  // Setup hot module replacement
-  module.hot.accept('./routes/index', () =>
-    setImmediate(() => {
-      ReactDOM.unmountComponentAtNode(MOUNT_NODE)
-      render(translationMessages)
-    })
-  )
 }
-
 
 // Chunked polyfill for browsers without Intl support
 if (!window.Intl) {
@@ -168,7 +99,6 @@ if (!window.Intl) {
   }))
     .then(() => Promise.all([
       import('intl/locale-data/jsonp/en.js'),
-      import('intl/locale-data/jsonp/de.js'),
     ]))
     .then(() => render(translationMessages))
     .catch((err) => {

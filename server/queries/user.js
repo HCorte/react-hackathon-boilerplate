26815ae14a2ab90a58169d5a22e6391441bd6ba5
Jan-Jan <jan.jan.code@gmail.com>
2017-05-02 13:01:04 +0200

    fixed toLowerCase

--- server/queries/user.js
+++ server/queries/user.js
@@ -3,20 +3,24 @@ const {
   pick,
   compose,
   evolve,
-  toLower,
 } = require('ramda')
 const { generateHash } = require('../middleware/user')
+
+const toLowerCase = str => typeof str === 'string'
+  ? str.toLowerCase()
+  : str
 
 const _getUser = query => {
   const cleanQuery = compose(
     evolve({
-      email: toLower,
-      username: toLower,
+      email: toLowerCase,
+      username: toLowerCase,
     }),
     pick(['_id', 'username', 'email'])
   )(query)
 
-  console.warn(`getUser: cleanQuery =`, cleanQuery)
+  console.error(`\nERROR: _getUser NOT implemented\n`)
+  console.warn(`_getUser: cleanQuery =`, cleanQuery)
 
   return Promise.resolve({
     _id: 'safkj13th3pj1pu4gh1p3u94gbq',

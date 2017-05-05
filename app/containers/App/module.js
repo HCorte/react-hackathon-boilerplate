194c78257bb46a512d7504bc44ea8270c770e702
Jan-Jan <jan.jan.code@gmail.com>
2017-05-05 18:33:52 +0200

    something broke with Observable fromPromise :'(

--- app/containers/App/module.js
+++ app/containers/App/module.js
@@ -1,7 +1,16 @@
 import changeCase from 'change-case'
 import { map } from 'rxjs/operator/map'
 
-
+/**
+ * commandEpic
+ * Listens `COMMAND` actions,
+ * then emits a `command` event via socket
+ * then emits a "converted-command" redux action
+ *
+ * eg, { type: `COMMAND`, payload: { type: `doSomething` }}
+ * causes socket.emit(`command`, { type: doSomething })
+ * and returns { type: `DO_SOMETHING` }
+ */
 export const commandEpic = socket => action$ =>
   action$.ofType(`COMMAND`)
     ::map(({ payload: command }) => {

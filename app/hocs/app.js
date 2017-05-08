e6b1833e95b4c7263e7417ec16e0938e3251ca27
Jan-Jan <jan.jan.code@gmail.com>
2017-05-08 18:16:43 +0200

    cleanup

--- app/hocs/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * app combines all the hocs required at the lowest level of the app
- */
-import pipe from 'lodash/fp/pipe'
-import me from './me'
-import cq from './commandAndQuery'
-
-export default pipe(me, cq)
